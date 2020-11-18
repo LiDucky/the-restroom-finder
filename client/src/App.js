70071badf17ba9b11d040a3974591a134476a46e
NinjaDuck9000 <mvroom3@gmail.com>
2020-11-17 16:05:12 -0800

    Working on menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Link, Router} from '@reach/router';
 import Map from './components/Map';
 
 function App() {
 
     return (
-        <div className="container">
-            <Map/>
+        <div className="center">
+            <div className="App">
+                <div className="topMenu">
+                    <div className="menuButton">
+                        
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-caret-down-fill" fill="currentColor"></svg>
+                    </div>
+                    <input type="text" name="search" placeholder="Search..."/>
+                </div>
+                <Map/>
+            </div>
         </div>
     );
 }
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { render } from 'react-dom';
-import {TileLayer, MapContainer, Marker } from 'react-leaflet';
+import {TileLayer, MapContainer, Marker, ZoomControl } from 'react-leaflet';
 
 const Map = props => {
     const center = [37.8697494, -122.3017989];
@@ -11,10 +11,13 @@ const Map = props => {
 
     return(
         <MapContainer 
+            zoomControl={false}
+            style={{ height: "100%" }}
             center = {center}
             zoom = {zoom} 
             id = "berkeleyMap"
         >
+            <ZoomControl position="topright"/>
             <TileLayer 
                 attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' 
                 url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
