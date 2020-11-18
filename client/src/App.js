3e809beee936e16daba64cc5791a1ed7dbe2c194
Yuri Chalmers <chalmers.yuri@gmail.com>
2020-11-17 16:04:09 -0800

    added markers, searchBar in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Main from './views/Main';
 import Map from './components/Map';
+import SearchBar from './components/SearchBar';
 
 function App() {
 
     return (
         <div className="container">
-            <Map/>
+            <Main />
         </div>
     );
 }
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,30 +1,61 @@
 import React from 'react';
-// import { render } from 'react-dom';
 import {TileLayer, MapContainer, Marker } from 'react-leaflet';
+// import data from './safelist.json'
+import axios from 'axios'
+import {useEffect, useState} from 'react'
+import MarkerPopup from './MarkerPopup'
+import SearchBar from './SearchBar'
+import Main from '../views/Main'
+
+
 
 const Map = props => {
-    const center = [37.8697494, -122.3017989];
-    const zoom = 13;
+    const {datalist} = props;
+    const center = [37.871576, -122.273029];
+    const zoom = 14;
+
+    // const [datalist, setDatalist] = useState([]);
     
 
 
 
+
+
     return(
-        <MapContainer 
-            center = {center}
-            zoom = {zoom} 
-            id = "berkeleyMap"
-        >
-            <TileLayer 
-                attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' 
-                url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            />
-            <Marker 
-                position = {[37.8697494, -122.3017989]}
-            />
-        </MapContainer>
+            <MapContainer 
+                center = {center}
+                zoom = {zoom} 
+                id = "berkeleyMap"
+            >
+                <TileLayer 
+                    attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' 
+                    url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                />
+                <Marker 
+                    position = {[37.871576, -122.273029]}
+                />
+                {
+                    datalist.map((item, i)=>(
+                        <>
+                            <Marker key = {i} 
+                                position = {[item.latitude, item.longitude]} 
+                            >
+                            <MarkerPopup 
+                                name = {item.name} 
+                                changing_table = {item.changing_table} 
+                                accessible = {item.accessible} 
+                                unisex = {item.unisex} 
+                                street = {item.street}
+                                city = {item.city}
+                                state = {item.state}
 
-
+                                />
+                            </Marker>
+                            <SearchBar datalist={datalist}/>
+                        </>
+                    ))
+                }
+            </MapContainer>
     )
 }
 export default Map;
